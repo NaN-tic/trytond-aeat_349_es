07291c3d1ec7c7317340a306180c32b346d46e66
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-07-22 12:57:55 +0200

    Cleanup tests

--- tests/test_aeat_349_es.py
+++ tests/test_aeat_349_es.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
+from trytond.tests.test_tryton import test_depends
 
 
 class Aeat349EsTestCase(unittest.TestCase):
@@ -35,6 +26,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             Aeat349EsTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
